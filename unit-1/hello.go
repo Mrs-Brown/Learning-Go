3be006d6b3f1d5f2489a6b13554b1f098cf23380
tea <tea.brown@outlook.com>
2022-09-27 12:49:09 -0800

    first commit

---
+++ unit-1/hello.go
@@ -0,0 +1,12 @@
+package func main    //Declares the package the code belongs to
+
+import (             //Makes the format (fmt) package available for use
+    "fmt"
+)
+
+func main() {        //Declares function called main
+    fmt.Println("Hello, World!")         //Prints to screen
+}
+
+//No semicolons? format of open bracket, code, close bracket
+//on each line will insert the semicolons during compilation
