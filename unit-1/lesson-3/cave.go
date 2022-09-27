1951a4695441d0d90ae654add69bb99e28712270
tea <tea.brown@outlook.com>
2022-09-27 14:06:38 -0800

    finished cave.go

--- unit-1/lesson-3/cave.go
+++ unit-1/lesson-3/cave.go
@@ -1,2 +1,15 @@
 //Function that returns Boolean values
 package main
+
+import (
+    "fmt"
+    "strings"
+)
+
+func main() {
+    fmt.Println("You find yourself in a dimly lit cave.")
+    var command = "walk outside"
+    var exit = strings.Contains(command, "outside")
+
+    fmt.Println("You leave the cave:", exit)  //Prints "You leave the cave: true
+}
