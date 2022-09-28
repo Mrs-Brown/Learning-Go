7cb6d41753c34390e6291c5eca93a02cf287af14
tea <tea.brown@outlook.com>
2022-09-28 12:08:57 -0800

    switch statements

---
+++ unit-1/lesson-3/switch.go
@@ -0,0 +1,20 @@
+//using switch statement
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("There is a cavern entrance here and a path to the east.")
+    var command = "go inside"
+
+    switch command {
+    case "go east":
+        fmt.Println("You head further up the mountain.")
+    case "enter cave", "go inside":   //A comma seperated list of possible values
+        fmt.Println("You find yourself in a dimly lit cave.")
+    case "read sign":
+        fmt.Println("The sign reads 'No Minors'.")
+    default:
+        fmt.Println("Didn't quite get that.")
+    }
+}
