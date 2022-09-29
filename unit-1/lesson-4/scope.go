07d1d43dd49d319742e4e9dcdc16923baebcf42f
tea <tea.brown@outlook.com>
2022-09-29 07:40:09 -0800

    scope example

--- unit-1/lesson-4/scope.go
+++ unit-1/lesson-4/scope.go
@@ -1,1 +1,17 @@
-//placeholder
+//Example of scope using a for loop
+package main
+
+import (
+    "fmt"
+    "math/rand"
+)
+
+func main() {
+    var count = 0
+
+    for count < 10 {  //Scope begins
+        var num = rand.Intn(10) + 1
+        fmt.Println(num)
+        count++
+    }  //Scope ends
+}
