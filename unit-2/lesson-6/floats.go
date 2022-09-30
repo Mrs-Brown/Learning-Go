45b2ee5033e686600d3e5fdc98dbdca491b6c39d
tea <tea.brown@outlook.com>
2022-09-30 11:35:22 -0800

    floating point numbers

--- unit-2/lesson-6/floats.go
+++ unit-2/lesson-6/floats.go
@@ -1,1 +1,15 @@
 //Example using floating-point numbers
+package main
+
+import (
+    "fmt"
+    "math"
+)
+
+func main() {
+    var pi64 = math.Pi
+    var pi32 float32 = math.Pi
+
+    fmt.Println(pi64) //3.141592653589793
+    fmt.Println(pi32) //3.1415927
+}
