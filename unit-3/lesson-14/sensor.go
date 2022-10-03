a5c2412cfb85e9c5c3185c413f1febf4e317d2e5
tea <tea.brown@outlook.com>
2022-10-03 13:25:30 -0800

    Assigning function to variable

---
+++ unit-3/lesson-14/sensor.go
@@ -0,0 +1,25 @@
+//Assign functions to variables
+package main
+
+import (
+    "fmt"
+    "math/rand"
+)
+
+type kelvin float64
+
+func fakeSensor() kelvin {
+    return kelvin(rand.Intn(151) +150) //Pseudorandom var for fake sensor results
+}
+
+func realSensor() kelvin {
+    return 0 //Will implement once sensor is obtained for Mars
+}
+
+func main() {
+    sensor := fakeSensor //Assign function to variable
+    fmt.Println(sensor())
+
+    sensor = realSensor //Assign function to same variable
+    fmt.Println(sensor())
+}
