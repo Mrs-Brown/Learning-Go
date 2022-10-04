0b295ac870c8f10433c60a32e18c3b960b85c09f
tea <tea.brown@outlook.com>
2022-10-04 14:34:27 -0800

    example of a simple chessboard

---
+++ unit-4/lesson-16/chess.go
@@ -0,0 +1,16 @@
+//Array of array of strings
+package main
+
+import "fmt"
+
+func main() {
+    var board [8][8]string  //An array of eight arrays of eight strings
+
+    board[0][0] = "r"
+    board[0][7] = "r"  //Places a rook at a [row][column] coordinate
+
+    for column := range board[1] {
+        board[1][column] = "p"
+    }
+    fmt.Print(board)
+}
