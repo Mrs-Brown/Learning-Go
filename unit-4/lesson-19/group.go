d1b3dc6524ae7482b643878481be1869950de056
tea <tea.brown@outlook.com>
2022-10-05 15:48:45 -0800

    Grouping data with maps and slices

---
+++ unit-4/lesson-19/group.go
@@ -0,0 +1,24 @@
+// Grouping data with maps and slices
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	temperatures := []float64{
+		-28.0, 32.0, -31.0, -29.0, -23.0, -29.0, -28.0, -33.0,
+	}
+
+	groups := make(map[float64][]float64) //A map with float64 keys and []float64 values
+
+	for _, t := range temperatures {
+		g := math.Trunc(t/10) * 10 //Rounds temperatures down to -20, -30, etc
+		groups[g] = append(groups[g], t)
+	}
+
+	for g, temperatures := range groups {
+		fmt.Printf("%v: %v\n", g, temperatures)
+	}
+}
