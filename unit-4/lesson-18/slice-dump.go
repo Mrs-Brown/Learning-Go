110c15a026a9049676c0e771072da981e9c7fd31
tea <tea.brown@outlook.com>
2022-10-05 11:34:16 -0800

    Array length amd capacity functions

---
+++ unit-4/lesson-18/slice-dump.go
@@ -0,0 +1,17 @@
+// Dump slice length, capacity, and contents
+package main
+
+import (
+	"fmt"
+)
+
+// Dump slice length, capacity, and contents
+func dump(label string, slice []string) {
+	fmt.Printf("%v: length %v, capacity %v %v\n", label, len(slice), cap(slice), slice)
+}
+
+func main() {
+	dwarfs := []string{"Ceres", "Pluto", "Haumea", "Makemake", "Eris"}
+	dump("dwarfs", dwarfs)           //Prints dwarfs: length 5, capacity 5 [contents]
+	dump("dwarfs[1:2]", dwarfs[1:2]) //Prints dwarfs[1:2]: length 1, capacity 4 [Pluto]
+}
