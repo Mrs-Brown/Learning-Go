fd6d95d9663b51fe07ae9a6cf730568794a666ba
tea <tea.brown@outlook.com>
2022-10-06 15:41:49 -0800

    initialize structs with composite literals

---
+++ unit-5/lesson-21/struct-literal.go
@@ -0,0 +1,16 @@
+// Initialize structures with composite literals will default to zero
+package main
+
+import "fmt"
+
+func main() {
+	type location struct {
+		lat, long float64
+	}
+
+	opportunity := location{lat: -1.9462, long: 354.4734}
+	fmt.Println(opportunity)
+
+	insight := location{lat: 4.5, long: 135.9}
+	fmt.Println(insight)
+}
