3a8740d0c066dc71162c177da6df93bf8b194193
tea <tea.brown@outlook.com>
2022-10-07 15:01:08 -0800

    Encoding in json again

---
+++ unit-5/lesson-21/landing.go
@@ -0,0 +1,29 @@
+// Program that displays three landing sites of different rovers in JSON
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+func main() {
+	type location struct { //Defining JSON structs
+		Name string  `json:"name"`
+		Lat  float64 `json:"latitude"`
+		Long float64 `json:"longitude"`
+	}
+	locations := []location{ //Slice of struct
+		{Name: "Bradbury Landing", Lat: -4.5895, Long: 137.4417},
+		{Name: "Columbia Memorial Station", Lat: -14.5684, Long: 175.472636},
+		{Name: "Challenger Memorial Station", Lat: -1.9462, Long: 354.4734},
+	}
+
+	bytes, err := json.MarshalIndent(locations, "", "  ")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println(string(bytes))
+}
